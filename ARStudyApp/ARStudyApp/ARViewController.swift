c532f2f96b282ae306b7c0ad22ffbccad0c8d9ab
尾原徳泰 <nori.tukareta@gmail.com>
2023-05-10 23:54:38 +0900

    view追加

---
+++ ARStudyApp/ARStudyApp/ARViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ARViewController.swift
+//  ARStudyApp
+//
+//  Created by Oh!ara on 2023/05/10.
+//
+
+import UIKit
+import ReplayKit
+import ARKit
+
+class ARViewController: UIViewController {
+    
+
+    @IBOutlet weak var arView: ARViewCont!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
